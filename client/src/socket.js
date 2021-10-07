d463451824576f6a7c49046548cd17e74197d400
elumu <eclair.lumukayembe@playtech.com>
2021-10-07 17:14:32 +0100

    - Fixed Bug: immediately add new message to chat UI for both existing and new conversation and display messages in correct order on page load

--- client/src/socket.js
+++ client/src/socket.js
@@ -6,7 +6,7 @@ import {
   addOnlineUser,
 } from "./store/conversations";
 
-const socket = io(window.location.origin);
+const socket = io("http://localhost:8000/");
 
 socket.on("connect", () => {
   console.log("connected to server");
